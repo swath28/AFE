6e83c3a3638670eb0dbca680ba7b17c78046386b
swath28 <153583816+swath28@users.noreply.github.com>
2024-03-13 18:41:50 +0530

    Update App.java

--- TicTacToe/codes/App.java
+++ TicTacToe/codes/App.java
@@ -1,23 +1,25 @@
 import board.Board;
 import java.util.Scanner;
 import player.Player;
+import game.Game;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("First Project-->Tic Tac Toe");
-              Scanner scn=new Scanner(System.in);
-              System.out.println("Enter name and symbol");
-              String name=scn.nextLine();
-              char symbol=scn.nextLine().charAt(0);
-              
-         Board b =new Board(4,'-');
+             //Scanner scn=new Scanner(System.in);
+             // System.out.println("Enter name and symbol");
+              //String name=scn.nextLine();
+             //char symbol=scn.nextLine().charAt(0);
+               
+         Board b =new Board(3,'-');
              b.printBoardConf(); 
             Player p1=new Player();
-         p1.setNameSymbol(name,symbol);
+         p1.setNameSymbol("Ramya",'O');
          p1.getNameSymbol(); 
           Player p2=new Player(); 
-          p2.setPlayerDetails("Swathi",19,'X',"9550333089","swath@gmail.com","abc");
-          p2.getPlayerDetails();
-
-          
+          p2.setNameSymbol("Swathi",'X');
+          p2.getNameSymbol();
+           Game game=new Game(new Player[] {p1,p2},b);
+           game.play();
+         
     }
 }
